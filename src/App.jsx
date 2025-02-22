68572d534c841893e9fc6fee9aacbbab7a284c72
Mansi agarwal <mansiagarwal293@gmail.com>
2025-02-22 14:21:25 +0530

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <div>
+      <Box>
+        <Text>This is App Component</Text>
+      </Box>
+    </div>
+  );
+};
+
+export default App;
